fbcf7c9ba3fd7ab54a39ebe495c84cb46f0589f7
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-05-26 09:29:12 -0700

    updated front end

--- lab-project-front/src/App.js
+++ lab-project-front/src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 const App = () => {
+
   return (
    <main>
-     <h1>Tops Ten '10' albums of all time</h1>
+     <h1>Best Albums of All Time</h1>
      <section>
        <form>
-        Image:<img src='<input type="text"/>'/><br/>
-        Name: <input type='text'/><br/>
-        Artist: <input type='text'/><br/>
-        Genre: <input type='text'/><br/>
-        year realeased: <input type='text'/><br/>
-        <input type = "submit" value="Creat Album"/><br/>
+         <input placeholder='Image Link' type='text'/><br/>
+         <input placeholder='Album Name' type='text'/><br/>
+         <input placeholder='Artist Name' type='text'/><br/>
+         <input placeholder='Genre' type='text'/><br/>
+         <input placeholder='Year Released' type='text'/><br/>
+         <input type = "submit" value='Create Album'/><br/>
        </form>
       </section>
    </main>
